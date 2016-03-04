182189cc0d096ddbef4beb0d1c884089aaf2f63b
Justin Chen <jchen978@gmail.com>
2016-03-03 18:32:53 -0600

    fixed bugsi n langevin_1d, now computes potentials and constructs hepsilon correctly. Updated run.py accordingly

--- examples/example_2/run.py
+++ examples/example_2/run.py
@@ -1,34 +1,63 @@
 """ Example for analyzing a 1d langevin dynamics model"""
+import numpy as np
+import scipy.stats as stats
 
-import pyfexd.model_loaders as ml
-import pyfexd.observables as observables
+import pyfexd
+ml = pyfexd.model_loaders
+observables = pyfexd.observables
+ene = pyfexd.max_likelihood.estimate_new_epsilons
 
 
 #load the model and load the data per the model's load_data method.
-lmodel = ml.Langevin("config.ini")
-data = lmodel.load_data("iteration_0/positions.dat")
+lmodel = ml.Langevin("simple.ini")
+data = lmodel.load_data("iteration_0/position.dat")
 
 #load the observable object that calculates the observables of a set of simulation data
-obs = observables.Obs()
-obs.add_histogram("observed_histogram.dat", nbins=400, histrange=(-20.0,20.0), error_type="gaussian") #load and format the data distribution
+edges = np.loadtxt("edges.dat")
+obs = observables.ExperimentalObservables()
+obs.add_histogram("exp_data.dat", edges=edges, errortype="gaussian") #load and format the data distribution
 
-obsval, obsstd = obs.compute_observations(data, range=(-20.0, 20.0), nbins=400)
+#obsval, obsstd = obs.compute_observations(data, range=(-20.0, 20.0), nbins=400)
 
 
 #do a simple discretizaiton fo the data into equilibrium distribution states.
 
-slices = obs.observables[0].slices
+hist, tempedges, slices = stats.binned_statistic(data, np.ones(np.shape(data)[0]), statistic="sum")
+ 
 
 possible_slices = np.arange(np.min(slices), np.max(slices)+1)
 
 equilibrium_frames = []
-
+indices = np.arange(np.shape(data)[0])
 for i in possible_slices:
-    data[slices == i]
-    equilibrium_frames.append(data)
-
+    state_data = indices[slices == i]
+    if not state_data.size == 0:
+        equilibrium_frames.append(state_data)
 
 
 
 #Now we can compute the set of epsilons that satisfy the max-likelihood condition
-import max_likelihood.estimate_new_epsilons
+#neweps = ene(data, equilibrium_frames, obs, lmodel)
+
+
+#debug
+new_eps, old_eps = ene(data, equilibrium_frames,obs,lmodel)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- model_loaders/langevin_1d.py
+++ model_loaders/langevin_1d.py
@@ -19,16 +19,16 @@ class Langevin(Model_Loader):
             ini_file_name = ini_file_name[:-4]
         self.model = lmodel(ini_file_name)
         
-        indices = np.arange(0, self.model.number_parameters)
-        self.use_params = indices[self.model.fit_parameters] # indices corresponding to potentials to use
-        self.epsilons = self.model.params[self.model.fit_parameters]
+        #indices = np.arange(0, self.model.number_parameters)
+        self.use_params = np.where(self.model.fit_parameters)[0] # get indices corresponding to potentials to use
+        self.epsilons = self.model.params[self.use_params]
         self.beta = 1.0
         
     def load_data(self,fname):
         return np.loadtxt(fname)
     
     def get_epsilons(self):
-        return self.model.params[self.model.fit_parameters]
+        return self.epsilons
     
     def get_potentials_epsilon(self, data):
         """ Takes a 1-d array, outputs a function(epsilons_list) 
@@ -46,15 +46,14 @@ class Langevin(Model_Loader):
         
         for i in self.use_params:
             constants_list.append(self.model.potential_functions[i](data))
-            
 
         
         def hepsilon(epsilons):
             #returns an array for the value of H for each frame, given a set of epsilons.
             total = np.zeros(np.shape(data)[0])
             
-            for i in self.use_params:
-                total = epsilons[i]*constants_list[i]
+            for i in range(np.shape(epsilons)[0]):
+                total += epsilons[i]*constants_list[i]
             
             total = np.exp(-1.0 * self.beta * total)
             
