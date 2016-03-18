71d4c7cba051cba2ef81b7bf2342a64744dfddf3
Justin Chen <jchen978@gmail.com>
2016-03-18 18:14:40 -0500

    Updated example_2 with a working fitting example.

--- examples/example_2/run.py
+++ examples/example_2/run.py
@@ -14,24 +14,16 @@ lmodel = ml.Langevin("simple.ini")
 iteration = lmodel.model.iteration
 data = lmodel.load_data("iteration_%d/position.dat" % iteration)
 
-print "Epsilons started as:"
-print lmodel.epsilons
 
 #load the observable object that calculates the observables of a set of simulation data
 edges = np.loadtxt("edges.dat")
 obs = observables.ExperimentalObservables()
 obs.add_histogram("exp_data.dat", edges=edges, errortype="gaussian") #load and format the data distribution
 
-#obsval, obsstd = obs.compute_observations(data, range=(-20.0, 20.0), nbins=400)
-
-
 #do a simple discretizaiton fo the data into equilibrium distribution states.
-
+#In theory, the user will be able to specify any sort of equlibrium states for their data
 hist, tempedges, slices = stats.binned_statistic(data, np.ones(np.shape(data)[0]), statistic="sum")
- 
-
 possible_slices = np.arange(np.min(slices), np.max(slices)+1)
-
 equilibrium_frames = []
 indices = np.arange(np.shape(data)[0])
 for i in possible_slices:
@@ -39,20 +31,17 @@ for i in possible_slices:
     if not state_data.size == 0:
         equilibrium_frames.append(state_data)
 
-print np.shape(equilibrium_frames)
 
 
 
 #Now we can compute the set of epsilons that satisfy the max-likelihood condition
-#neweps = ene(data, equilibrium_frames, obs, lmodel)
-
-
-#debug
-solutions = ene(data, equilibrium_frames, obs, lmodel)
+#set logq=True for using the logarithm functions
+#currently, options are simplex, annealing, cg
+solutions = ene(data, equilibrium_frames, obs, lmodel, solver="simplex", logq=False)
 new_eps = solutions.new_epsilons
 old_eps = solutions.old_epsilons
-Qold = solutions.Qfunction_epsilon(old_eps)
-Qnew = solutions.Qfunction_epsilon(new_eps)
+Qold = solutions.oldQ
+Qnew = solutions.newQ
 
 print "Epsilons are: "
 print new_eps
@@ -66,13 +55,6 @@ savestr = "iteration_%d/newton" % iteration
 if not os.path.isdir(savestr):
     os.mkdir(savestr)
 np.savetxt("%s/params" % savestr, np.append([1.0], new_eps))
-
-
-
-
-
-
-
 
 
 
