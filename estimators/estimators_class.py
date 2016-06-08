Major bug fix. Improved performance of Q function calculation. Number of Derivative function evaluations is significantly reduced

estimators/estimators_class.py
@@ -93,7 +93,6 @@ class EstimatorsObject(object):
             self.expectation_observables.append(observed)
             self.epsilons_functions.append(epsilons_function)
             self.derivatives_functions.append(derivatives_function)
-            
             self.h0.append(epsilons_function(self.current_epsilons))
             
             self.ni.append(num_in_set)
@@ -123,6 +122,11 @@ class EstimatorsObject(object):
         t2 = time.time()
         total_time = (t2-t1) / 60.0
         print "Initializaiton Completed: %f Minutes" % total_time 
+        
+        self.count_Qcalls = 0
+        self.count_hepsilon = 0
+        self.count_dhepsilon = 0
+        
     def get_reweighted_observable_function(self):
         return self.calculate_observables_reweighted
         
@@ -150,6 +154,7 @@ class EstimatorsObject(object):
         #calculate re-weighting for all terms 
         for i in range(self.number_equilibrium_states):
             next_weight = np.sum(np.exp(self.epsilons_functions[i](epsilons) - self.h0[i])) / self.ni[i]
+            self.count_hepsilon += 1
             next_observed += next_weight * self.state_prefactors[i]
             total_weight += next_weight * self.pi[i]
         
@@ -230,14 +235,18 @@ class EstimatorsObject(object):
         
         dQ_vector = np.array(dQ_vector)
         
+        ##debug
+        self.count_Qcalls += 1
         return Q, dQ_vector
     
     def get_derivative_pieces(self, epsilons, boltzman_weights):
         derivative_observed_first = [np.zeros(self.num_observable) for j in range(self.number_params)]
         derivative_observed_second = [np.zeros(self.num_observable) for j in range(self.number_params)]
         for i in range(self.number_equilibrium_states):
+            deriv_function = self.derivatives_functions[i](epsilons)
+            self.count_dhepsilon += 1
             for j in range(self.number_params):
-                next_weight_derivatives = np.sum(boltzman_weights[i] * self.derivatives_functions[i](epsilons)[j]) / self.ni[i]
+                next_weight_derivatives = np.sum(boltzman_weights[i] * deriv_function[j]) / self.ni[i]
                 derivative_observed_first[j] += next_weight_derivatives * self.state_prefactors[i]
                 derivative_observed_second[j] += next_weight_derivatives * self.pi[i]
         
@@ -270,6 +279,7 @@ class EstimatorsObject(object):
             boltzman_weights = []
             for i in range(self.number_equilibrium_states):
                 boltzman_wt = np.exp(self.epsilons_functions[i](epsilons) - self.h0[i])
+                self.count_hepsilon += 1
                 boltzman_weights.append(boltzman_wt)
         except:
             print "Exponential Function Failed"
@@ -278,6 +288,7 @@ class EstimatorsObject(object):
             max_val = 0
             for i in range(self.number_equilibrium_states):
                 exponent = self.epsilons_functions[i](epsilons) - self.h0[i]
+                self.count_hepsilon += 1
                 max_exponent = np.max(exponent)
                 if max_exponent > max_val:
                     max_val = max_exponent

model_loaders/proteins.py
@@ -12,6 +12,7 @@ try:
 except:
     pass
 
+
 class Protein(ModelLoader):
     """ Subclass for making a ModelLoader for a Protein Model
     
@@ -108,12 +109,13 @@ class Protein(ModelLoader):
             total = np.zeros(np.shape(data)[0])
             for i in range(np.shape(epsilons)[0]):
                 total += epsilons[i]*constants_list[i]
-            
+
             return total     
         
         #compute the function for the derivative of the potential energy
         def dhepsilon(epsilons):
             #first index is corresponding epsilon, second index is frame
+
             return constants_list
         
         return hepsilon, dhepsilon
@@ -142,15 +144,15 @@ class ProteinNonLinear(Protein):
             for i in range(np.shape(epsilons)[0]):
                 total += functions_list[i](epsilons[i])
             total *= -1. * self.beta
-            
+
             return total     
         
         #compute the function for the derivative of the potential energy
         def dhepsilon(epsilons):
             #first index is corresponding epsilon, second index is frame
-            derivatives_list = []
-            for func in dfunctions_list:
-                derivatives_list.append(func(epsilons) * -1. * self.beta)
+            scaled_beta = -1. * self.beta
+            derivatives_list = [func(epsilons)*scaled_beta for func in dfunctions_list]
+
             return derivatives_list
         
         return hepsilon, dhepsilon