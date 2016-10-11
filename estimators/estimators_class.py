03601d4f2384fdb65f3c5bf8b8e46051ead1945d
Justin Chen <jchen978@gmail.com>
2016-10-11 15:24:21 -0500

    added print statement when saving debug files

--- estimators/estimators_class.py
+++ estimators/estimators_class.py
@@ -3,6 +3,7 @@
 import numpy as np
 import time
 import mdtraj as md
+import os
 
 np.seterr(over="raise")
 
@@ -388,7 +389,8 @@ class EstimatorsObject(object):
         file2: trace of Q values during optmization
         
         """
-        
+        cwd = os.getcwd
+        "Saving Debug files to %s" % cwd
         f = open("function_calls.dat", "w")
         f.write("Number of times Q was computed: %d\n" % self.count_Qcalls)
         f.write("Number of times the Hamiltonian was computed: %d\n" % self.count_hepsilon)
