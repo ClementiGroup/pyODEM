2e0820124879d99d8aac68a59ac7ae4d33ca8630
Justin Chen <jchen978@gmail.com>
2017-01-08 20:17:11 -0600

    updated examples so they run in a more reasonable time

--- examples/example_2/multirun.sh
+++ examples/example_2/multirun.sh
@@ -8,7 +8,7 @@ for i in `seq 1 1 $num`; do
 
 echo $starting simulation run $i
 
-time python -m langevin_model.simulation sim --name $name --steps 1000000
+time python -m langevin_model.simulation sim --name $name --steps 100000
 time python -m run
 python -m langevin_model.simulation next --name $name --start
 
