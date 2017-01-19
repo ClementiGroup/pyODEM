67e27dd33d94cf43f6f73e7553cd35b6787bd53f
Justin Chen <jchen978@gmail.com>
2017-01-18 19:58:59 -0600

    fixed so that histograms with zero observations don't turn into nans when normalized

--- observables/hist_analysis_pkg.py
+++ observables/hist_analysis_pkg.py
@@ -23,24 +23,15 @@ class HistogramO(Observable):
                 hist, edges = np.histogram(x, weights=weights, range=[((xmin*spacing)/(xmax*spacing))], bins=xmax-xmin)
         else:
             hist, edges = np.histogram(data, weights=weights, bins=self.edges)
-        
+
         assert not np.any(np.isnan(hist))
         normalization = math.fabs(integrate_simple(hist, edges))
         assert not np.any(np.isnan(normalization))
-        print normalization
-        print hist
-        with np.errstate():
-            try:
-                stdev = np.sqrt(hist) / normalization
-                hist = hist / normalization
-            except:
-                raise
-                print normalization
-                print hist
-                print np.sqrt(hist)
-        print hist
-        stdev = np.sqrt(hist) / normalization
-        hist = hist / normalization
+        if normalization == 0: #no observed states in the range of interest
+            stdev = hist + 1
+        else:
+            stdev = np.sqrt(hist) / normalization
+            hist = hist / normalization
         bincenters = 0.5*(edges[1:] + edges[:-1])
 
         seen = [not i==0 for i in hist]
