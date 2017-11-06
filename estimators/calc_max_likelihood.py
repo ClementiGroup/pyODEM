090167b129cc40fb4d2799a6c7dd4575e1fcc995
TensorDuck <jchen978@gmail.com>
2017-11-06 12:22:00 -0800

    bug fix

--- estimators/calc_max_likelihood.py
+++ estimators/calc_max_likelihood.py
@@ -121,7 +121,7 @@ def max_likelihood_estimate(data, dtrajs, observables, model, obs_data=None, sol
                 if all_obs_data is None:
                     all_obs_data = [obs_value for obs_value in thing]
                 else:
-                    all_obs_data = [np.append(all_obs_value, obs_value, axis=0) for all_obs_values,obs_value in zip(all_obs_data,thing)]
+                    all_obs_data = [np.append(all_obs_value, obs_value, axis=0) for all_obs_value,obs_value in zip(all_obs_data,thing)]
 
             for thing in all_obs_data:
                 assert np.shape(thing)[0] == nframes_total # sanity check
