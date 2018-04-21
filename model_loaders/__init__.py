cd46957ec6a0210c99396574938a920a878955b1
Justin Chen <jchen978@gmail.com>
2018-04-20 19:27:46 -0500

    added method to helper_functions.py to load and parse histogram obs_data

--- model_loaders/__init__.py
+++ model_loaders/__init__.py
@@ -36,3 +36,4 @@ from proteins import ProteinNonBonded
 
 #load all the helper functions
 from helper_functions import load_protein_nb
+from helper_functions import load_distance_traces
--- model_loaders/helper_functions.py
+++ model_loaders/helper_functions.py
@@ -5,7 +5,7 @@ import mdtraj as md
 from proteins import ProteinNonBonded
 import pyODEM.basic_functions.util as util
 
-def load_protein_nb(topf, dtrajs, traj_files, top_file):
+def load_protein_nb(topf, dtrajs, traj_files, top_file, observable_object=None, obs_data=None):
     comm = MPI.COMM_WORLD
 
     size = comm.Get_size()
@@ -31,7 +31,42 @@ def load_protein_nb(topf, dtrajs, traj_files, top_file):
 
     collected_data = []
     for state_idx in use_states:
+        stuff = {}
+        stuff["index"] = state_idx
         data = pmodel.load_data(traj[all_indices[state_idx]])
-        collected_data.append([state_idx, data])
+        stuff["data"] = data
+        if observable_object is not None:
+            if obs_data is None:
+                use_obs_data = [data for i in range(len(observable_object.observables))]
+            else:
+                use_obs_data = []
+                for obs_dat in obs_data:
+                    use_obs_data.append(obs_dat[all_indices[state_idx]])
 
-    return collected_data
+            observed, obs_std = observable_object.compute_observations(use_obs_data)
+            stuff["obs_result"] = observed
+            stuff["obs_std"] = obs_std
+
+        collected_data.append(stuff)
+
+
+    return pmodel, collected_data
+
+def load_distance_traces(traj_file, top_file, fit_pairs):
+    traj = md.load(traj_file, top=top_file)
+
+    top = traj.top
+
+    ca_names = "name CA"
+    residue_codes = "GAVLIMFWPSTCYNQDEKRH"
+    for count in range(20):
+        ca_names += " or name CA%s" % (residue_codes[count])
+    all_ca_idxs = top.select(ca_names) # convert residue index to ca index
+
+    all_observed = []
+    for residue_pair in fit_pairs:
+        ca_pairs = [all_ca_idxs[residue_pair[0]], all_ca_idxs[residue_pair[1]]]
+        distance_trace = md.compute_distances(traj, [ca_pairs], periodic=False)[:,0]
+        all_observed.append(distance_trace)
+
+    return all_observed
