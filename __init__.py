6587ee6b6c0a3f61e3b7db2e85370b3d93fe38f4
Justin Chen <jchen978@gmail.com>
2019-02-26 17:46:37 -0600

    moved MPI initialization to a new Init function for pyODEM.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 """ Initiate all the sub packages for calculating things """
-
+import numpy as np
+from mpi4py import MPI
 
 from . import basic_functions
 
@@ -7,3 +8,16 @@ from . import model_loaders
 from . import observables
 
 from . import estimators
+
+
+
+def Init():
+    """ Optional (often required) for initializing MPI and numpy in parallel """
+
+    try:
+        MPI.Init()
+    except:
+        pass
+
+    # should implement a way to seed numpy.random that is effective.
+    # Google: numpy parallelization random seed for details.
