53477f2379f6e506578fcb54055da0441d47291b
Iryna Zaporozhets <iz2@rice.edu>
2020-01-17 23:50:29 -0600

    Removed temporary code

--- ddG_estimators/ddG_estimate.py
+++ ddG_estimators/ddG_estimate.py
@@ -483,11 +483,7 @@ class ddG(Observable):
         H_mutated, d_H_mutated = self._compute_mutated_H(epsilons,compute_derivative=compute_derivative)
         exp_delta_H = np.exp(self._compute_delta_H(H0,H_mutated))
         exp_delta_H_micro_aver = self._get_microstate_averages(exp_delta_H)
-        aver_folded = self._get_ensemble_averages(microstates_folded,exp_delta_H_micro_aver,reweighted=reweighted,epsilons=epsilons)
-        print(aver_folded)
         aver_folded = self._get_ensemble_averages2(microstates_folded,exp_delta_H_micro_aver,reweighted=reweighted,epsilons=epsilons)
-        print(aver_folded)
-        assert (self._get_ensemble_averages(microstates_folded,exp_delta_H_micro_aver,reweighted=reweighted,epsilons=epsilons)-self._get_ensemble_averages2(microstates_folded,exp_delta_H_micro_aver,reweighted=reweighted,epsilons=epsilons))<0.0000001
         aver_unfolded = self._get_ensemble_averages2(microstates_unfolded,exp_delta_H_micro_aver,reweighted=reweighted,epsilons=epsilons)
         delta_delta_G = -1*np.log(aver_folded) + np.log(aver_unfolded)
         if self.rescale_temperature:
