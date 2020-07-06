7a1a6e61a7f63de83a81c9a04b2355c4ddbec219
Iryna Zaporozhets <iz2@rice.edu>
2020-07-06 11:17:51 -0500

    Updated __init__ with Q_function_2 and Mutational_model_linear

--- ddG_estimators/__init__.py
+++ ddG_estimators/__init__.py
@@ -6,3 +6,5 @@ from . import Estimator
 from . import Q_function
 from . import ddG_estimate_depricated
 from . import ddG_estimate_linear
+from . import Q_function_2
+from . import Mutational_model_linear
