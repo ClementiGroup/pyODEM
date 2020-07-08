95b9906cd9d92d8f12986cc38c14e62c4430dedb
Iryna Zaporozhets <iz2@rice.edu>
2020-07-07 23:24:13 -0500

    Working version

--- ddG_estimators/likelihood_functions.py
+++ ddG_estimators/likelihood_functions.py
@@ -124,5 +124,6 @@ class Likelihood():
             assert len(observed_values) == len(calculated_values), "Length of experimental and calculated observable values do not match"
             z_score = self._compute_z_score(calculated_values,std,observed_values)
             negative_lnQ +=  0.5*np.sum(np.square(z_score))
-            derivative_negative_lnQ += np.sum(np.multiply(np.divide(z_score,std),calculated_derivatives),axis=1)
+            product = np.multiply(np.divide(z_score,std),calculated_derivatives.T)
+            derivative_negative_lnQ += np.sum(product,axis=1)
         return negative_lnQ, derivative_negative_lnQ
