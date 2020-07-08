aaec5edac14c7a880b72830f4fef23f16f638f45
Iryna Zaporozhets <iz2@rice.edu>
2020-07-07 20:28:26 -0500

    Added likelihood function to the __init__ file

--- ddG_estimators/__init__.py
+++ ddG_estimators/__init__.py
@@ -6,5 +6,5 @@ from . import Estimator
 from . import Q_function
 from . import ddG_estimate_depricated
 from . import ddG_estimate_linear
-from . import Q_function_2
+from . import likelihood_functions
 from . import Mutational_model_linear
