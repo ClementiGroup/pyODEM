4144ce6622f06e82d8d03744123562ecd7d1442e
ZiZ1 <iz2@rice.edu>
2021-02-11 17:20:07 +0100

    Add tests for AWSEM model Hamiltonian calcukations

--- test/model_loaders_awsem_protein_test.py
+++ test/model_loaders_awsem_protein_test.py
@@ -24,9 +24,6 @@ class TestAWSEMProtein():
     and derivatives
     """
 
-    def __init__(self):
-        pass
-
     def test_direct_contacts(self):
         traj = md.load(f'{DATA_PATH}/movie.pdb')
         topology = traj.top
@@ -41,13 +38,35 @@ class TestAWSEMProtein():
         my_protein = ml.AWSEMProtein(topology)
         my_protein.load_data(traj)
         my_protein._compute_pairwise_distances(traj)
-        direct_interaction = ml.DirectInteraction(sequence)
+        direct_interaction = ml.DirectInteraction(len(sequence))
         direct_interaction.load_paramters(f'{DATA_PATH}/gamma.dat')
-        params, types = direct_interaction.get_all_parameters()
-        derivs, unique_params = direct_interaction.calculate_derivatives(my_protein.distances)
-        print(unique_params)
-        H_computed = np.sum(np.multiply(derivs,unique_params), axis = 1)
-        assert (numpy.isclose(H_ref, H_computed, rtol=1e-4))
-# test = TestAWSEMProtein()
-# H  = test.test_direct_contacts()
-# print(H)
+        derivs = direct_interaction.calculate_derivatives(sequence,
+                                                         input=my_protein.distances)
+        params = direct_interaction.get_parameters()
+        H_computed = np.sum(np.multiply(derivs, params), axis = 1)
+        assert np.isclose(H_ref, H_computed,  atol=1e-05).all()
+
+
+    def test_get_potentials_epsilon(self):
+        traj = md.load(f'{DATA_PATH}/movie.pdb')
+        topology = traj.top
+        openawsem_protein = ml.OpenAWSEMProtein()
+        openawsem_protein.prepare_system(
+                           f'{DATA_PATH}/1pgb_openmmawsem.pdb',
+                           os.path.abspath(f'{DATA_PATH}/.'),
+                           [contact_term],
+                           sequence,
+                           chains='A')
+        H_ref = openawsem_protein.calculate_H_for_trajectory(traj)
+        my_protein = ml.AWSEMProtein(topology, parameter_location=f'{DATA_PATH}/.')
+        my_protein.load_data(traj)
+        my_protein.setup_Hamiltonian(terms=['direct'])
+        H_func, _ = my_protein.get_potentials_epsilon(sequence)
+        params = my_protein.get_epsilons()
+        H_calculated = -1.0*H_func(params) # Returns negative value
+        assert np.isclose(H_ref, H_calculated, atol=1e-5).all()
+        return
+
+#test = TestAWSEMProtein()
+#test.test_direct_contacts()
+#test.test_get_potentials_epsilon()
