d04f4cf3f91ef2fde1cb78d44b3283cd9a6a41ab
ZiZ1 <iz2@rice.edu>
2021-02-12 21:49:59 +0100

    Add test for mediated contacts

--- test/model_loaders_awsem_protein_test.py
+++ test/model_loaders_awsem_protein_test.py
@@ -30,21 +30,46 @@ class TestAWSEMProtein():
         openawsem_protein = ml.OpenAWSEMProtein()
         openawsem_protein.prepare_system(
                            f'{DATA_PATH}/1pgb_openmmawsem.pdb',
-                           os.path.abspath(f'{DATA_PATH}/.'),
+                           os.path.abspath(f'{DATA_PATH}/params_direct_only/.'),
                            [contact_term],
                            sequence,
                            chains='A')
         H_ref = openawsem_protein.calculate_H_for_trajectory(traj)
         my_protein = ml.AWSEMProtein(topology)
         my_protein.load_data(traj)
-        my_protein._compute_pairwise_distances(traj)
+        #my_protein._compute_pairwise_distances(traj)
         direct_interaction = ml.DirectInteraction(len(sequence))
-        direct_interaction.load_paramters(f'{DATA_PATH}/gamma.dat')
+        direct_interaction.load_paramters(f'{DATA_PATH}/params_direct_only/gamma.dat')
         derivs = direct_interaction.calculate_derivatives(sequence,
                                                          input=my_protein.distances)
         params = direct_interaction.get_parameters()
         H_computed = np.sum(np.multiply(derivs, params), axis = 1)
         assert np.isclose(H_ref, H_computed,  atol=1e-05).all()
+
+
+    def test_mediated_contacts(self):
+        traj = md.load(f'{DATA_PATH}/movie.pdb')
+        topology = traj.top
+        openawsem_protein = ml.OpenAWSEMProtein()
+        openawsem_protein.prepare_system(
+                           f'{DATA_PATH}/1pgb_openmmawsem.pdb',
+                           os.path.abspath(f'{DATA_PATH}/params_mediated_only/.'),
+                           [contact_term],
+                           sequence,
+                           chains='A')
+        #openawsem_protein.oa.
+        H_ref = openawsem_protein.calculate_H_for_trajectory(traj)
+        my_protein = ml.AWSEMProtein(topology)
+        my_protein.load_data(traj)
+        mediated_interaction = ml.MediatedInteraction(len(sequence))
+        mediated_interaction.load_paramters(f'{DATA_PATH}/params_mediated_only/gamma.dat')
+        derivs = mediated_interaction.calculate_derivatives(sequence, my_protein.distances, my_protein.local_density)
+        params = mediated_interaction.get_parameters()
+        H_computed = np.sum(np.multiply(derivs, params), axis = 1)
+        print(H_ref)
+        print(H_computed)
+        assert np.isclose(H_ref, H_computed,  atol=1e-05).all()
+
 
 
     def test_get_potentials_epsilon(self):
@@ -53,12 +78,12 @@ class TestAWSEMProtein():
         openawsem_protein = ml.OpenAWSEMProtein()
         openawsem_protein.prepare_system(
                            f'{DATA_PATH}/1pgb_openmmawsem.pdb',
-                           os.path.abspath(f'{DATA_PATH}/.'),
+                           os.path.abspath(f'{DATA_PATH}/params_direct_only/.'),
                            [contact_term],
                            sequence,
                            chains='A')
         H_ref = openawsem_protein.calculate_H_for_trajectory(traj)
-        my_protein = ml.AWSEMProtein(topology, parameter_location=f'{DATA_PATH}/.')
+        my_protein = ml.AWSEMProtein(topology, parameter_location=f'{DATA_PATH}/params_direct_only/.')
         my_protein.load_data(traj)
         my_protein.setup_Hamiltonian(terms=['direct'])
         H_func, _ = my_protein.get_potentials_epsilon(sequence)
@@ -67,6 +92,7 @@ class TestAWSEMProtein():
         assert np.isclose(H_ref, H_calculated, atol=1e-5).all()
         return
 
-#test = TestAWSEMProtein()
+test = TestAWSEMProtein()
 #test.test_direct_contacts()
+test.test_mediated_contacts()
 #test.test_get_potentials_epsilon()
