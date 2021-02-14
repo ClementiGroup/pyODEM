8a6e2cf1ab8ad3b5dad865aaf85f9c2a7c989b2e
ZiZ1 <iz2@rice.edu>
2021-02-14 20:01:08 +0100

    Add burial contacts test

--- test/model_loaders_awsem_protein_test.py
+++ test/model_loaders_awsem_protein_test.py
@@ -69,6 +69,31 @@ class TestAWSEMProtein():
         print(H_ref)
         print(H_computed)
         assert np.isclose(H_ref, H_computed,  atol=1e-05).all()
+        return
+
+
+    def test_burial_contacts(self):
+        traj = md.load(f'{DATA_PATH}/movie.pdb')
+        topology = traj.top
+        openawsem_protein = ml.OpenAWSEMProtein()
+        openawsem_protein.prepare_system(
+                           f'{DATA_PATH}/1pgb_openmmawsem.pdb',
+                           os.path.abspath(f'{DATA_PATH}/params_burial_only/.'),
+                           [contact_term],
+                           sequence,
+                           chains='A')
+        H_ref = openawsem_protein.calculate_H_for_trajectory(traj)
+        my_protein = ml.AWSEMProtein(topology)
+        my_protein.load_data(traj)
+        mediated_interaction = ml.BurialInteraction(len(sequence))
+        mediated_interaction.load_paramters(f'{DATA_PATH}/params_burial_only/burial_gamma.dat')
+        derivs = mediated_interaction.calculate_derivatives(sequence, my_protein.local_density)
+        params = mediated_interaction.get_parameters()
+        H_computed = np.sum(np.multiply(derivs, params), axis = 1)
+        print(H_ref)
+        print(H_computed)
+        assert np.isclose(H_ref, H_computed,  atol=1e-05).all()
+        return
 
 
 
@@ -78,14 +103,14 @@ class TestAWSEMProtein():
         openawsem_protein = ml.OpenAWSEMProtein()
         openawsem_protein.prepare_system(
                            f'{DATA_PATH}/1pgb_openmmawsem.pdb',
-                           os.path.abspath(f'{DATA_PATH}/params_direct_only/.'),
+                           os.path.abspath(f'{DATA_PATH}/params_all/.'),
                            [contact_term],
                            sequence,
                            chains='A')
         H_ref = openawsem_protein.calculate_H_for_trajectory(traj)
-        my_protein = ml.AWSEMProtein(topology, parameter_location=f'{DATA_PATH}/params_direct_only/.')
+        my_protein = ml.AWSEMProtein(topology, parameter_location=f'{DATA_PATH}/params_all/.')
         my_protein.load_data(traj)
-        my_protein.setup_Hamiltonian(terms=['direct'])
+        my_protein.setup_Hamiltonian(terms=['direct','mediated','burial'])
         H_func, _ = my_protein.get_potentials_epsilon(sequence)
         params = my_protein.get_epsilons()
         H_calculated = -1.0*H_func(params) # Returns negative value
@@ -94,5 +119,5 @@ class TestAWSEMProtein():
 
 test = TestAWSEMProtein()
 #test.test_direct_contacts()
-test.test_mediated_contacts()
+test.test_burial_contacts()
 #test.test_get_potentials_epsilon()
