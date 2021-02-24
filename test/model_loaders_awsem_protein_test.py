6310a9ecd1a142dba9ae467c57b587d715bd3c03
ZiZ1 <iz2@rice.edu>
2021-02-24 13:03:44 +0100

    Add test for SBM nonbonded contacts in hamiltonian_terms module.

--- test/model_loaders_awsem_protein_test.py
+++ test/model_loaders_awsem_protein_test.py
@@ -17,6 +17,26 @@ from helperFunctions.myFunctions import *
 DATA_PATH = 'test_data/awsem_sample_data'
 sequence = 'MTYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE'
 
+
+def prepare_energy_function(pmodel, data, fraction=None):
+     """
+     Here, prepare an energy function in a format used by
+     ddG_generic class. Need to unwrap it back to 2d array
+     """
+     Q = []
+     # Need to use a random set of parameters. For safety, make all parameters
+     # equal to 1
+     epsilon = pmodel.get_epsilons()
+     epsilon = np.ones(len(epsilon))
+     for dictionary in data:
+         state = dictionary['index']
+         values = np.array(dictionary['data'])
+         _, depsilons = pmodel.get_potentials_epsilon(values)
+         q_microstate = np.array(depsilons(epsilon)).T
+         Q.append(q_microstate)
+     derivative_array = np.concatenate(Q, axis=0)
+     return(derivative_array)
+
 class TestAWSEMProtein():
     """
     Test model loader, that loads information for AWSEM contact
@@ -38,7 +58,7 @@ class TestAWSEMProtein():
         my_protein = ml.AWSEMProtein(topology)
         my_protein.load_data(traj)
         #my_protein._compute_pairwise_distances(traj)
-        direct_interaction = ml.DirectInteraction(len(sequence))
+        direct_interaction = ml.AWSEMDirectInteraction(len(sequence))
         direct_interaction.load_paramters(f'{DATA_PATH}/params_direct_only/gamma.dat')
         derivs = direct_interaction.calculate_derivatives(sequence,
                                                          input=my_protein.distances)
@@ -61,7 +81,7 @@ class TestAWSEMProtein():
         H_ref = openawsem_protein.calculate_H_for_trajectory(traj)
         my_protein = ml.AWSEMProtein(topology)
         my_protein.load_data(traj)
-        mediated_interaction = ml.MediatedInteraction(len(sequence))
+        mediated_interaction = ml.AWSEMMediatedInteraction(len(sequence))
         mediated_interaction.load_paramters(f'{DATA_PATH}/params_mediated_only/gamma.dat')
         derivs = mediated_interaction.calculate_derivatives(sequence, my_protein.distances, my_protein.local_density)
         params = mediated_interaction.get_parameters()
@@ -85,7 +105,7 @@ class TestAWSEMProtein():
         H_ref = openawsem_protein.calculate_H_for_trajectory(traj)
         my_protein = ml.AWSEMProtein(topology)
         my_protein.load_data(traj)
-        mediated_interaction = ml.BurialInteraction(len(sequence))
+        mediated_interaction = ml.AWSEMBurialInteraction(len(sequence))
         mediated_interaction.load_paramters(f'{DATA_PATH}/params_burial_only/burial_gamma.dat')
         derivs = mediated_interaction.calculate_derivatives(sequence, my_protein.local_density)
         params = mediated_interaction.get_parameters()
@@ -93,6 +113,31 @@ class TestAWSEMProtein():
         print(H_ref)
         print(H_computed)
         assert np.isclose(H_ref, H_computed,  atol=1e-05).all()
+        return
+
+
+    def test_sbm_nonbonded_contacts(self):
+        sbm_data_path = 'test_data/CA_sample_data'
+        traj_file = f'{sbm_data_path}/CA_sample_traj.xtc'
+        top_file = f'{sbm_data_path}/ref_CA.pdb'
+        param_description_file = f'{sbm_data_path}/pairwise_params'
+        param_value_file =  f'{sbm_data_path}/model_params'
+        model_name = f'{sbm_data_path}/ubq.ini'
+        dtrajs = np.loadtxt(f'{sbm_data_path}/dtrajs.txt', dtype=int)
+        traj = md.load(traj_file, top=top_file)
+        topology = traj.top
+        # Put standart model loader here
+        pmodel, data_formatted = pyODEM.model_loaders.load_protein(dtrajs,
+                                                                    traj_file,
+                                                                    model_name,
+                                                                    observable_object=None,
+                                                                    obs_data=None)
+        reference_derivatives = -1.0*prepare_energy_function(pmodel, data_formatted)
+        my_protein = ml.HybridProtein(topology=topology, traj_type='sbm_ca')
+        my_protein.load_data(traj)
+        sbm_nonbonded_interaction = ml.SBMNonbondedInteraction(topology.n_residues, param_description_file)
+        derivatives = sbm_nonbonded_interaction.calculate_derivatives(my_protein.distances)
+        assert np.isclose(reference_derivatives, derivatives).all()
         return
 
 
@@ -117,7 +162,33 @@ class TestAWSEMProtein():
         assert np.isclose(H_ref, H_calculated, atol=1e-5).all()
         return
 
+
+    def test_get_potentials_epsilon_hybrid(self):
+        """
+        The test just check if everything is called without errors, and
+        shapes of the outputs is as expected.
+        For now, does not check if actual calculations are correct.
+
+        """
+        # Loading required data
+        sbm_data_path = 'test_data/CA_sample_data'
+        traj_file = f'{sbm_data_path}/CA_sample_traj.xtc'
+        top_file = f'{sbm_data_path}/ref_CA.pdb'
+        traj = md.load(traj_file, top=top_file)
+        param_description_file = f'{sbm_data_path}/pairwise_params'
+        param_value_file =  f'{sbm_data_path}/model_params'
+        model_name = f'{sbm_data_path}/ubq.ini'
+        my_protein = ml.HybridProtein(topology=traj.topology, traj_type='sbm_ca', parameter_location=f'{sbm_data_path}/.')
+        my_protein.load_data(traj)
+        my_protein.setup_Hamiltonian(terms=['sbm_nonbonded', 'burial'])
+        ubq_seq = 'MQIFVKTLTGKTITLEVEPSDTIENVKAKIQDKEGIPPDQQRLIFAGKQLEDGRTLSDYNIQKESTLHLVLRLRGG'
+        H_func = my_protein.get_H_func(sequence=ubq_seq, fraction=None)
+        params = my_protein.get_epsilons()
+        assert len(params) == 65 # Correct number of parameters, 5 sbm + 60 from burial terms
+        H, dH = H_func(params, return_derivatives=True)
+        assert len(H) == traj.n_frames
+        assert dH.shape == (traj.n_frames, 65)
+        return
+
 test = TestAWSEMProtein()
-#test.test_direct_contacts()
-test.test_burial_contacts()
-#test.test_get_potentials_epsilon()
+test.test_get_potentials_epsilon_hybrid()
