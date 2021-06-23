5e62bd59a283ae600f2e9b4a0507b1cee372d413
ZiZ1 <zaporozhets.i.o@gmail.com>
2021-06-23 11:30:07 +0200

    Bug fix

--- model_loaders/hybrid_model.py
+++ model_loaders/hybrid_model.py
@@ -234,7 +234,7 @@ class HybridProtein(ModelLoader):
         where each pair of aminoacid types has a unique model parameter
         """
         sbm_residue_specific_interaction = SBMNonbondedInteractionsByResidue(func_type ='auto')
-        sbm_residue_specific_intreaction.load_topology(f'{self.parameter_location}/ref.pdb')
+        sbm_residue_specific_interaction.load_topology(f'{self.parameter_location}/ref.pdb')
         sbm_residue_specific_interaction.load_parameter_description(f'{self.parameter_location}/pairwise_params', mode='full')
         sbm_residue_specific_interaction.load_parametes(f'{self.parameter_location}/model_params')
         # The distances that exist in HybridProtein (self.distances) are not the same that need to be used
